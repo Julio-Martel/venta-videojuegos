8b305dfc4035e5fc848765e517aafc16107bd85a
Julio-Martel <julioramdom123@gmail.com>
2025-12-23 22:10:42 -0300

    se ha comentado y se han agregado comentarios como autoayuda para cuando se retome el codigo

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+
+/*AGREGAR LA LOGICA DE CAMBIO DE IMAGENES*/
+
+const idImagen = document.getElementById('nro-imagen-1').id;
+const partesId = idImagen.split("-");
+
 const slide = {
-    botonDerecho: document.getElementById('flecha-uno'),
-    botonIzquierdo: document.getElementById('flecha-dos')
+  botonDerecho: document.getElementById('flecha-uno'),
+  botonIzquierdo: document.getElementById('flecha-dos')
+};
+
+
+if(partesId[2] === "1") {
+    slide.botonDerecho.style.opacity = "0.5";
+    slide.botonDerecho.style.pointerEvents = "none";
+} else {
+
 }
 
+
+//slide.botonDerecho.addEventListener('click', );
