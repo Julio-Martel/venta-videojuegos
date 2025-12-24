6ffe52f16989a11dba31f578139ace180002f7cf
Julio-Martel <julioramdom123@gmail.com>
2025-12-24 19:03:51 -0300

    se ha agregado la logica del cambio de imagen

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 
 /*AGREGAR LA LOGICA DE CAMBIO DE IMAGENES*/
 
-const idImagen = document.getElementById('nro-imagen-1').id;
-const partesId = idImagen.split("-");
+const imagen = document.getElementById("nro-imagen-1");
 
-const slide = {
-  botonDerecho: document.getElementById('flecha-uno'),
-  botonIzquierdo: document.getElementById('flecha-dos')
-};
+function actualizarEstado() {
+  const partesId = imagen.id.split("-");
 
-
-if(partesId[2] === "1") {
+  if (partesId[2] === "1") {
     slide.botonDerecho.style.opacity = "0.5";
     slide.botonDerecho.style.pointerEvents = "none";
-} else {
 
+    slide.botonIzquierdo.style.opacity = "1";
+    slide.botonIzquierdo.style.pointerEvents = "auto";
+  } else {
+    slide.botonIzquierdo.style.opacity = "0.5";
+    slide.botonIzquierdo.style.pointerEvents = "none";
+
+    slide.botonDerecho.style.opacity = "1";
+    slide.botonDerecho.style.pointerEvents = "auto";
+  }
 }
 
+const slide = {
+  botonDerecho: document.getElementById("flecha-uno"),
+  botonIzquierdo: document.getElementById("flecha-dos"),
+};
 
-//slide.botonDerecho.addEventListener('click', );
+actualizarEstado();
+
+slide.botonIzquierdo.addEventListener("click", () => {
+  imagen.id = "nro-imagen-2";
+  imagen.src = "./images/imagen2.jpg";
+  actualizarEstado();
+});
+
+slide.botonDerecho.addEventListener("click", () => {
+  imagen.id = "nro-imagen-1";
+  imagen.src = "./images/imagen1.jpg";
+  actualizarEstado();
+});
