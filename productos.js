e7d1a8d10a7579ebf804cd412c3d417e7c75c4ba
Julio-Martel <julioramdom123@gmail.com>
2025-12-31 22:32:46 -0300

    Se ha agregado un comentario de forma de autoayuda

--- productos.js
+++ productos.js
@@ -5,10 +5,10 @@ export const generarContenidoProductos = () => {
         contedorProductos: document.createElement('div'),
         totalCasillasProductos: 4,
         listadoProductos: [
-            {nombreVideojuego: 'Resident Evil Requiem', descripcion: '', precio: 53000},
-            {nombreVideojuego: 'Silent Hill F', descripcion: '', precio: 45000},
-            {nombreVideojuego: 'Battelfield 6', descripcion: '', precio: 50000},
-            {nombreVideojuego: 'Resident Evil 4', descripcion:'', precio: 25000}
+            {nombreVideojuego: 'Resident Evil Requiem', descripcion: '', precio: 53000, stock: 5},
+            {nombreVideojuego: 'Silent Hill F', descripcion: '', precio: 45000, stock: 8},
+            {nombreVideojuego: 'Battelfield 6', descripcion: '', precio: 50000, stock: 0},
+            {nombreVideojuego: 'Resident Evil 4', descripcion:'', precio: 25000, stock: 2}
         ]
     };
     
@@ -26,16 +26,30 @@ export const generarContenidoProductos = () => {
 
     for(let i = 0; i < productos.totalCasillasProductos; i++){
         const casilla = document.createElement('div');
-        
+        const elementosCasilla = {
+            nombreProducto: document.createElement('h1'),
+            imagenProducto: document.createAttribute('img'),
+            stock: document.createAttribute('div')
+        }
         
         casilla.classList.add('casilla');
         casilla.id = `casilla-${i}`;
 
-
-
-
+        elementosCasilla.nombreProducto.classList.add('nombre-producto');
+        elementosCasilla.imagenProducto.classList.add('imag-prod');
+        elementosCasilla.stock.classList.add('stock-producto');
+        
+        casilla.appendChild(elementosCasilla.nombreProducto);
+        casilla.appendChild(elementosCasilla.imagenProducto);
+        casilla.appendChild(elementosCasilla.stock);
+    
         productos.contedorProductos.appendChild(casilla);
     }
+
+
+/*
+    CAMBIAR LA FORMA DEL FOR PARA PODER IDENTIFICAR AL OBJETO PARA PODER COLOCAR SUS CARACTERISTICAS
+*/
 
     return contenidoProductoCarrito;
 
