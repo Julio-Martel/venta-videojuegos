c3f40cb4992f79ed80c243b4f473c94cc17c6dfb
Julio-Martel <julioramdom123@gmail.com>
2025-12-31 21:48:10 -0300

    se ha implementado correctamente el for para completar el contenedor de los productos

--- productos.js
+++ productos.js
@@ -1,15 +1,33 @@
 export const generarContenidoProductos = () => {
     const contenidoProductoCarrito = document.createElement('div');
-    const contedorProductos = document.createElement('div');
+    
+    const productos = {
+        contedorProductos: document.createElement('div'),
+        totalCasillasProductos: 4,
+        listadoProductos: {
+            
+        }
+    };
+    
     const carrito = document.createElement('div');
 
     contenidoProductoCarrito.classList.add('carrito-producto');
     contenidoProductoCarrito.id = "car-prod";
-    contenidoProductoCarrito.appendChild(contedorProductos);
+   
+    contenidoProductoCarrito.appendChild(productos.contedorProductos);
+   
     contenidoProductoCarrito.appendChild(carrito);
 
-    contedorProductos.classList.add('contedor-productos')
+    productos.contedorProductos.classList.add('contedor-productos')
     carrito.classList.add("contenedor-carrito");
+
+    for(let i = 0; i < productos.totalCasillasProductos; i++){
+        const casilla = document.createElement('div');
+        casilla.classList.add('casilla');
+        casilla.id = `casilla-${i}`;
+
+        productos.contedorProductos.appendChild(casilla);
+    }
 
     return contenidoProductoCarrito;
 
