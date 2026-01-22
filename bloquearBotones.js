46f51068409909e578e808caf47d1070ca865624
Julio-Martel <julioramdom123@gmail.com>
2026-01-22 18:54:16 -0300

    se ha agregado una parte a la condicional

--- bloquearBotones.js
+++ bloquearBotones.js
@@ -4,12 +4,17 @@ export const actualizarBotones =(stockActual, max, botonMenos, botonMas) => {
     botonMenos.style.pointerEvents = "none";
     botonMas.style.opacity = "1";
     botonMas.style.pointerEvents = "auto";
-  } else if (stockActual === max) {
+  } else if (stockActual === max && stockActual !== 0) {
     botonMas.style.opacity = "0.5";
     botonMas.style.pointerEvents = "none";
     botonMenos.style.opacity = "1";
     botonMenos.style.pointerEvents = "auto";
-  } else {
+  } else if(stockActual === 0){
+    botonMenos.style.opacity = "0.5";
+    botonMenos.style.pointerEvents = "none";
+    botonMas.style.opacity = "0.5";
+    botonMas.style.pointerEvents = "none";
+  }else {
     botonMenos.style.opacity = "1";
     botonMenos.style.pointerEvents = "auto";
     botonMas.style.opacity = "1";
