5ef1d1c94194c841cade2e9c5855aad44b09571b
Julio-Martel <julioramdom123@gmail.com>
2026-01-30 19:36:57 -0300

    se ha eliminado el parametro de la funcion y se han agregado nuevas clases a los nuevos contenedores interiores

--- listadoDeProductos.js
+++ listadoDeProductos.js
@@ -1,8 +1,14 @@
-export const listaDeProductosAgregadosAlCarrito = (todosLosProductosAñadidosAlCarrito) => {
+export const listaDeProductosAgregadosAlCarrito = () => {
     const contenedorMostrarListado = document.createElement('div');
-    
+    const contenedoresListadoPagar = {
+        lista: document.createElement('div'),
+        mostrarPrecioBotonPagar: document.createElement('div')
+    }
+
     contenedorMostrarListado.className = 'listado-productos';
-         
+    
+    contenedoresListadoPagar.lista.className = "lista";
+    contenedoresListadoPagar.mostrarPrecioBotonPagar.className = "contenedor-precio-boton";
 
 
 
