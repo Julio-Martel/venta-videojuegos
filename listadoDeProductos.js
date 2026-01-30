99d81b746ef25f6f7b5b467b6771fee0a93e382e
Julio-Martel <julioramdom123@gmail.com>
2026-01-30 19:27:18 -0300

    se ha añadido la clase principal

--- listadoDeProductos.js
+++ listadoDeProductos.js
@@ -1,3 +1,13 @@
-export const listaDeProductosAgregadosAlCarrito = () => {
+export const listaDeProductosAgregadosAlCarrito = (todosLosProductosAñadidosAlCarrito) => {
+    const contenedorMostrarListado = document.createElement('div');
     
+    contenedorMostrarListado.className = 'listado-productos';
+         
+
+
+
+
+    return contenedorMostrarListado;
+
+
 }
