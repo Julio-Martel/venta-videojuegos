5125675933630e94b4da0559aa5e5356b13e9a54
Julio-Martel <julioramdom123@gmail.com>
2026-02-14 19:30:39 -0300

    se ha agregado el boton de eliminar producto de agregar al carrito

--- listadoDeProductos.js
+++ listadoDeProductos.js
@@ -4,13 +4,17 @@ export const listaDeProductosAgregadosAlCarrito = () => {
         lista: document.createElement('div'),
         mostrarPrecioBotonPagar: document.createElement('div'),
         mostrarPrecio: document.createElement('div'),
-        botonPagar: document.createElement('button')
+        botonPagar: document.createElement('button'),
+        botonQuitarProducto: document.createElement('button')
     }
 
     contenedorMostrarListado.className = 'listado-productos';
     
     contenedoresListadoPagar.lista.className = "lista";
     contenedoresListadoPagar.mostrarPrecioBotonPagar.className = "contenedor-precio-boton";
+    contenedoresListadoPagar.botonQuitarProducto.className = "boton-quitar-producto";
+
+
 
     contenedorMostrarListado.append(contenedoresListadoPagar.lista, contenedoresListadoPagar.mostrarPrecioBotonPagar);
 
@@ -19,8 +23,9 @@ export const listaDeProductosAgregadosAlCarrito = () => {
     contenedoresListadoPagar.botonPagar.id = "pagar-btn"
 
     contenedoresListadoPagar.botonPagar.textContent = "Realizar pago!"
+    contenedoresListadoPagar.botonQuitarProducto.textContent = "Quitar producto del carrito"
 
-    contenedoresListadoPagar.mostrarPrecioBotonPagar.append(contenedoresListadoPagar.mostrarPrecio,contenedoresListadoPagar.botonPagar);
+    contenedoresListadoPagar.mostrarPrecioBotonPagar.append(contenedoresListadoPagar.mostrarPrecio, contenedoresListadoPagar.botonQuitarProducto,contenedoresListadoPagar.botonPagar);
 
     return contenedorMostrarListado;
 
